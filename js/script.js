ed19954cdb1bbad467f5491d3eb3c3dae814a71c
sacmario <marioc.trab@gmail.com>
2022-06-15 14:02:50 -0300

    Commit CRUD inicial

---
+++ js/script.js
@@ -0,0 +1,90 @@
+window.addEventListener('load', start);
+
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+var inputName = null;
+
+function start() {
+    inputName = document.querySelector('#inputName');
+    preventFormSubmit();
+    activateInput();
+    render();
+}
+
+function preventFormSubmit() {
+    function handleFormSubmit(event){
+        event.preventDefault();
+    }
+
+    var form = document.querySelector('form')
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+    function insertName(newName){
+        globalNames.push(newName)
+        console.log(globalNames)
+    }
+
+    function handleTyping(event) {
+        if (event.key === 'Enter') {
+            insertName(event.target.value);
+            render()
+        }
+    }
+    
+    inputName.addEventListener('keyup', handleTyping)
+    inputName.focus();
+}
+
+function render () {
+    function createDeleteButton(index) {
+        function deleteName(){            
+            globalNames.splice(index, 1);
+            render();
+        }
+        var button = document.createElement('button');
+        button.classList.add('deleteButton');
+        button.textContent = 'x';
+        button.addEventListener('click', deleteName)
+        return button;
+    }
+
+    function createSpan(name) {
+        function editItem() {
+            inputName.value = name
+            inputName.focus();
+        }
+        var span = document.createElement('span');
+        span.classList.add('clickable');
+        span.textContent = name;
+        span.addEventListener('click', editItem)
+        return span
+    }
+
+    var divNames = document.querySelector('#names');
+    divNames.innerHTML = '';
+    var ul = document.createElement('ul');
+
+    for (var i = 0; i < globalNames.length; i ++){
+        var currentName = globalNames[i];
+        var li = document.createElement('li');
+        var button = createDeleteButton(i)
+        var span = createSpan(currentName);
+
+        
+
+        li.appendChild(button);
+
+        li.appendChild(span)
+        ul.appendChild(li)
+    }   
+    divNames.appendChild(ul);
+    clearInput()
+    
+}
+
+function clearInput() {
+    inputName.value = '';
+    inputName.focus();
+}
+
